5422893777cef284e2ef0a74ff5bb2e4b169bf64
PaksF <flaksfjtm@gmail.com>
2024-05-23 11:39:31 +0900

    Controller 계증과 View 계층: 컨트롤러 매핑, list.html 수정

--- src/main/java/kr/ac/kopo/guestbook2024/controller/GuestbookController.java
+++ src/main/java/kr/ac/kopo/guestbook2024/controller/GuestbookController.java
@@ -1,15 +1,41 @@
 package kr.ac.kopo.guestbook2024.controller;
 
+import kr.ac.kopo.guestbook2024.dto.GuestbookDTO;
+import kr.ac.kopo.guestbook2024.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook2024.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
+@RequiredArgsConstructor
 public class GuestbookController {
+    private final GuestbookService service;
 
-    @GetMapping({"","/", "/list"})
-    public String list(){
-        return "/guestbook/list";
+    @GetMapping({"/"})
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping({"/list"})
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        // list.html(view 계층)에 발명록 목록과 화면에 보여질 때 필요한 페이지번호들 동의 정보를 전달
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping({"/register"})
+    public void register(){
+
+    }
+
+    @GetMapping({"/register"})
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        Long gno = service.register(dto);
+        redirectAttributes.addAttribute("msg", gno);
+        return "redirect:/guestbook/list";
     }
 }
